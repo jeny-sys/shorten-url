96bda2edf3145e11f6f00f823ae39fbad1d01419
Design <brainstorming@local>
2026-06-13 00:08:12 +0800

    fix: 加 @EnableScheduling 让 StatsWorker 真正跑起来

--- src/main/java/com/yourname/shorten/ShortenUrlApplication.java
+++ src/main/java/com/yourname/shorten/ShortenUrlApplication.java
@@ -2,8 +2,10 @@ package com.yourname.shorten;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ShortenUrlApplication {
 
 	public static void main(String[] args) {
